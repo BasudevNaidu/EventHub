29ad16d54a45edc0eed9fba48c071177db27baf0
BasudevNaidu <basudevnaidu2@gmail.com>
2025-03-19 17:42:49 +0530

    Some changes in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,20 +20,20 @@ const Footer = () => {
             <div className="space-y-2">
               <div className="flex items-center space-x-2 text-gray-400">
                 <Mail className="h-5 w-5" />
-                <span>contact@eventhub.com</span>
+                <span>basudevnaidu2@gmail.com</span>
               </div>
               <div className="flex items-center space-x-2 text-gray-400">
                 <Phone className="h-5 w-5" />
-                <span>+1 (555) 123-4567</span>
+                <span>+91 7848921602</span>
               </div>
             </div>
           </div>
           <div>
             <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-white">Twitter</a>
-              <a href="#" className="text-gray-400 hover:text-white">LinkedIn</a>
-              <a href="#" className="text-gray-400 hover:text-white">Facebook</a>
+              <a href="https://x.com/basudev_na59764" className="text-gray-400 hover:text-white">Twitter</a>
+              <a href="https://www.linkedin.com/in/naidu-basudev-96b7a6289/" className="text-gray-400 hover:text-white">LinkedIn</a>
+              <a href="https://github.com/BasudevNaidu" className="text-gray-400 hover:text-white">GitHub</a>
             </div>
           </div>
         </div>
